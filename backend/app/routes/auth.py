c19bb86d890f42a94b3a49feb93839550e0aac50
Aristarkh <your@email.com>
2026-04-21 08:34:25 +0000

    payme stable 2026-04-21

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,48 +1,9 @@
-from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
-from jose import jwt, JWTError
-from sqlmodel import Session
-from app.db import get_session
+from fastapi import APIRouter, Depends
 from app.models import User
-
+from app.security import get_current_user
 
 router = APIRouter()
 
-
-
-SECRET_KEY = "SECRET"
-ALGORITHM = "HS256"
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
-
-
-def get_current_user(
-    token: str = Depends(oauth2_scheme),
-    session: Session = Depends(get_session),
-):
-    credentials_exception = HTTPException(
-        status_code=401,
-        detail="Could not validate credentials",
-    )
-
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        user_id: int = payload.get("user_id")
-    except JWTError:
-        raise credentials_exception
-
-    user = session.get(User, user_id)
-
-    if not user:
-        raise credentials_exception
-
-    if user.is_banned:
-        raise HTTPException(
-        status_code=403,
-        detail=user.ban_reason or "Ваш аккаунт заблокирован"
-    )
-
-    return user
 
 @router.get("/me")
 def get_me(current_user: User = Depends(get_current_user)):
